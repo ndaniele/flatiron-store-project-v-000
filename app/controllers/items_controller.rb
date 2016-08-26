868083ebc9e04c5dd5a48ef4d40f9b81622cecb3
ndaniele <n.daniele@gmail.com>
2016-08-26 14:00:34 -0400

    Done.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,7 @@
 class ItemsController < ApplicationController
+   def show
+    @current_cart = current_user.current_cart ||= current_user.create_current_cart if user_signed_in?
+    @item = Item.find(params[:id])
+  end
 
 end
